2802042a4b7326be823cfe25b16d926ead0b491e
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-12-06 11:47:40 -0300

    product obj with props

--- src/routes/ProductDetails/index.tsx
+++ src/routes/ProductDetails/index.tsx
@@ -4,6 +4,27 @@ import ButtonPrimary from "../../components/ButtonPrimary";
 import HeaderClient from "../../components/HeaderCliet";
 import ProductDetailsCard from "../../components/ProductDetailsCard";
 
+import { ProductDTO } from '../../models/product';
+
+//Variavel temporaria
+const product: ProductDTO = {
+   id: 2,
+   name: "Smart Tv",
+   description: "Esta Tv é muito bonita",
+   imgUrl : "https://i.zst.com.br/thumbs/12/3e/37/1553975765.jpg",
+   price : 2500.99,
+   categories: [
+      {
+         id : 2,
+         name : "Eletronicos"
+      },
+      {
+         id : 1,
+         name : "Computadores"
+      }
+   ]
+}
+
 export default function ProductDetails() {
 
    return (
@@ -11,7 +32,7 @@ export default function ProductDetails() {
          <HeaderClient></HeaderClient>
          <main>
             <section id="product-details-section" className="dsc-container">
-               <ProductDetailsCard></ProductDetailsCard>
+               <ProductDetailsCard product={product} />
                <div className="dsc-btn-page-container">
                   <ButtonPrimary></ButtonPrimary>
                   <ButtonInverse></ButtonInverse>

56dda2ec258ba1cabe70dda65a60c1eb808f228c
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-12-07 14:57:08 -0300

    react router dom and links

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import Catalog from './routes/Catalog';
-import ProductDetails from './routes/ProductDetails';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ClientHome from './routes/ClientHome';
+import Catalog from './routes/ClientHome/Catalog';
+import ProductDetails from './routes/ClientHome/ProductDetails';
 
 function App() {
 
   return (
-    
-    <Catalog></Catalog>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ClientHome />}>
+            <Route index element={<Catalog />} />
+            <Route path="catalog" element={<Catalog />} />
+            <Route path="product-details/:productId" element={<ProductDetails />} />
+        </Route>
+
+
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
---
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -0,0 +1,33 @@
+import './style.css';
+import ButtonInverse from "../../../components/ButtonInverse";
+import ButtonPrimary from "../../../components/ButtonPrimary";
+import ProductDetailsCard from "../../../components/ProductDetailsCard";
+import * as productServices from '../../../services/product-services'
+import { useParams } from 'react-router-dom';
+
+export default function ProductDetails() {
+
+   const params = useParams();
+
+   const product = productServices.findById(Number(params.productId));
+
+
+   return (
+      <>
+         <main>
+            <section id="product-details-section" className="dsc-container">
+
+               {
+                  product &&
+                  <ProductDetailsCard product={product} />
+               }
+
+               <div className="dsc-btn-page-container">
+                  <ButtonPrimary text="Comprar" />
+                  <ButtonInverse text="Inicio" />
+               </div>
+            </section>
+         </main>
+      </>
+   );
+}
